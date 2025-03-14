5564d7bdcd7956e0e0ca06e57f44bea1960ece4b
Jacob Thygesen <info@jaxweb.dk>
2025-03-15 00:15:28 +0100

    Update functions.php
    Added support for forcing chache flush when updating style.css with filemtime()

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ function mytheme_setup() {
 add_action('after_setup_theme', 'mytheme_setup');
 
 function mytheme_enqueue_scripts() {
-    wp_enqueue_style('mytheme-style', get_stylesheet_uri());
+    wp_enqueue_style('mytheme-style', get_stylesheet_uri()), array(), filemtime( get_stylesheet_directory() . '/style.css' );
     wp_enqueue_script('mytheme-scripts', get_template_directory_uri() . '/script.js', array(), false, true);
 }
 add_action('wp_enqueue_scripts', 'mytheme_enqueue_scripts');
